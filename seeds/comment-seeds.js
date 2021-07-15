90c35a0a680cdb0abc2e59da7ebc6e2d12a26151
Alex Bright <abrighttt@gmail.com>
2021-07-15 01:06:34 -0400

    seeds fixed

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,7 +1,13 @@
 const { Comment } = require('../models');
 
 const commentData = [
-    {}
+
+    {
+        user_id: 3,
+        post_id: 5,
+        comment_text: "Fantastic!"
+    },
+    
 ]
 
 const seedComments = () => Comment.bulkCreate(commentData);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,12 +3,5 @@
       "username": "Alex",
       "password": "password12345"
     },
-    {
-      "username": "Aliya",
-      "password": "password12345"
-    },
-    {
-      "username": "Ashley",
-      "password": "password12345"
-    }
+    
   ]
