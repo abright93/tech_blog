78a239d98716b7aa99c974d0fca29f7c89636c20
Alex Bright <abrighttt@gmail.com>
2021-07-15 00:56:38 -0400

    files fixed

---
+++ seeds/user-seeds.js
@@ -0,0 +1,14 @@
+[
+    {
+      "username": "Alex",
+      "password": "password12345"
+    },
+    {
+      "username": "Aliya",
+      "password": "password12345"
+    },
+    {
+      "username": "Ashley",
+      "password": "password12345"
+    }
+  ]
